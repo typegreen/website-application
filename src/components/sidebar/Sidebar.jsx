b9e89f4b70098d9cf09a33ff87d17ce0fcd1c85e
typegreen <paulhumb2013@gmail.com>
2025-04-22 12:07:34 +0800

    Website Testing 9 -Adding back Capture Image  Integration

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -27,6 +27,15 @@ const Sidebar = () => {
 
           {!isAdmin() && (
             <li className='listItem'>
+              <NavLink to="/captured-image" className='menuLink flex'>
+                <RxActivityLog className='icon' />
+                <span className='smallText'>Capture Image</span>
+              </NavLink>
+            </li>
+          )}
+
+          {!isAdmin() && (
+            <li className='listItem'>
               <NavLink to="/detection-logs" className='menuLink flex'>
                 <RxActivityLog className='icon' />
                 <span className='smallText'>Detection Logs</span>
